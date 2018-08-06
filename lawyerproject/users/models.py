af8c675622ca45da6a9e4596fe67fa3ca6984894
iqbalrasel91 <raselbubt@gmail.com>
2018-08-06 12:18:05 +0600

    confict fix

--- lawyerproject/users/models.py
+++ lawyerproject/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from role.models import Role
 
 # Create your models here.
+
+
+class User(models.Model):
+    role = models.ForeignKey(Role, related_name='User', on_delete=models.CASCADE, null=True)
+    full_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+    status = models.IntegerField(blank=True,null=True)
+    created_by = models.IntegerField(blank=True,null=True)
+    created_at = models.DateField(blank=True,null=True)
+
+    def __str__(self):
+        return self.full_name
+
--- lawyerproject/users/urls.py
+++ lawyerproject/users/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='user_index'),
+    path('create', views.create, name='user_create'),
+    path('user_store', views.store, name='user_store'),
+    path('edit/<int:id>', views.edit, name='user_edit'),
+    path('update/<int:id>', views.update, name='user_update'),
+    path('delete/<int:id>', views.delete, name='user_delete'),
 
 ]
